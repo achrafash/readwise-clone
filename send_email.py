a3f66362dcf843db54c8d1e34970adda2e4ccc3a
Achraf Ash <53062630+AchrafAsh@users.noreply.github.com>
2019-07-23 12:31:30 +0200

    Added the instruction to log in email account

--- send_email.py
+++ send_email.py
@@ -2,3 +2,18 @@ import smtplib
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+s = smtplib.SMTP(host='smtp.office365.com', port=587)
+#s = smtplib.SMTP(host='smtp.gmail.com', port=465) for gmail address
+
+s.starttls()
+
+email_address = str(input('Enter your email address: '))
+password = str(input('Enter your password: '))
+
+#login to your email account
+s.login(email_address, passsword)
+
+# create a message
+msg = MIMEMultipart()
+
