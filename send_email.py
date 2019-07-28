8c9f2e95382b56de99e76a37e54deb333de97535
Achraf Ash <53062630+AchrafAsh@users.noreply.github.com>
2019-07-28 10:02:53 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,15 +1,12 @@
 import smtplib
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
 from highlights import get_library
 
 s = smtplib.SMTP(host='smtp.office365.com', port=587)
 #s = smtplib.SMTP(host='smtp.gmail.com', port=465) for gmail address
 
 s.starttls()
-
 email_address = # enter the email address from which the mail will be sent
 password = # enter the password of this address
 
@@ -20,8 +17,14 @@ s.login(email_address, passsword)
 msg = MIMEMultipart()
 
 message = ""
-# Code to get the highlights and organise them into message is going to go there
 library = get_library()
+for book in library:
+    if book.get_highlights():
+        message += str(book.get_title().decode("utf-8")) + '\n'
+        message += str(book.get_author().decode("utf-8")) + '\n\n'
+        for highlight in book.get_highlights():
+            message += str(highlight.decode("utf-8")) + '\n'
+        message += '_________________________________' + '\n'
 
 # setup the parameters of the message
 msg['From'] = email_address
@@ -33,7 +36,6 @@ msg.attach(MIMEText(message))
 
 # send the message via the server set up earlier.
 s.send_message(msg)
-
+print('The email has been successfully sent')
 del msg
-
 s.quit()
